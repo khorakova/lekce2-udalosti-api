74dd70e5eb180abf5bf7c043224d24d513bbd2b8
khorakova <horakoak@gmail.com>
2023-01-30 15:24:38 +0100

    reseni ulohy

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
-// tady je místo pro náš program
+// tučné písmo v odstavci po najetí myši na odstavec
+let odstavec = document.querySelector(".odstavec");
+
+function priNajeti() {
+    odstavec.style.fontWeight = "bold";
+} 
+
+function priOpusteni() {
+    odstavec.style.fontWeight = "normal";
+}
+
+// kliknuti na button zmeni pismo v odstavci na cervene, opakovane stisknuti odebere cervenou barvu
+function cervenyPoKliknuti() {
+    odstavec.classList.toggle("cervenyOdstavec");
+}
+
+// po kliknutí zvětší velikost písma v odstavci o jeden px
+
+function vetsiPoKliknuti() { 
+    let id = document.getElementById("odstavec");
+    style = window.getComputedStyle(id, null).getPropertyValue("font-size");
+    currentSize = parseInt(style);
+    currentSize++;
+    id.style.fontSize = currentSize.toString() + "px";
+}
+
+// prehravac
+let prehravac = document.querySelector(".mujPrehravac");
+
+function Play() {
+    prehravac.play();
+}
+
+function Pause() {
+    prehravac.pause();
+}
+
+function Reset() {
+    prehravac.currentTime = 0;
+}
+
+function Volume0() {
+    prehravac.volume = 0;
+}
+
+function Volume05() {
+    prehravac.volume = 0.5;
+}
+
+function Volume1() {
+    prehravac.volume = 1;
+}
